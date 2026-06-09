aa115449f01550f38ecdea7941a978fc5ed2106c
DavidAlejandro123 <alejandroariste210@gmail.com>
2026-06-09 10:55:24 -0300

    Ejemplo de Try Catch

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Excepciones: Eventos que interrumpen el flujo normal de la aplicacion
+
+        int numero1 = 5;
+        int numero2 = 0;
+        int resultado;
+
+        try{
+            resultado = numero1/numero2;
+            System.out.println(resultado);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Esto va despues de la excepcion");
+        }
+    }
+}
