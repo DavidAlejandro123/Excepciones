c1cc86625953a74d8dc53fd73ff80afa6dd9095d
DavidAlejandro123 <alejandroariste210@gmail.com>
2026-06-09 11:33:24 -0300

    checked exception, try catch

--- src/App.java
+++ src/App.java
@@ -1,15 +1,19 @@
+import Exceptions.DividirPorCeroException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         //Excepciones: Eventos que interrumpen el flujo normal de la aplicacion
         //Excepciones de tipo unchecked
-        int numero1 = 5;
+        int numero1 = 10;
         int numero2 = 0;
         int resultado;
 
+        Calculadora calculadora = new Calculadora();
+
         try{
-            resultado = numero1/numero2;
+            resultado = calculadora.dividir(numero1,numero2);
             System.out.println(resultado);
-        } catch (Exception e){
+        } catch (DividirPorCeroException e){
             e.printStackTrace();
             System.out.println("Esto va despues de la excepcion");
         }
